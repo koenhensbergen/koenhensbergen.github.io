28a6e4ba8819c0a212b653913c95ec250d63a18c
koenhensbergen <55744070+koenhensbergen@users.noreply.github.com>
2020-02-05 13:36:21 +0100

    Add files via upload

---
+++ portfolio/script/script.js
@@ -0,0 +1,13 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('nav ul');
+    
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        
+        burger.classList.toggle('active');        
+        
+    });
+}
+
+navSlide();
